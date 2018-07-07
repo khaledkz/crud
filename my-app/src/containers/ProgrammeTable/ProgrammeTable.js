1ee5248acebdaf0d9574ac184b9ff259726d6fc1
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-07 11:30:01 +0100

    display table list

--- my-app/src/containers/ProgrammeTable/ProgrammeTable.js
+++ my-app/src/containers/ProgrammeTable/ProgrammeTable.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
+import { Table, Alert } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import './ProgrammeTable.css';
+import MockData from '../../data/data'
 
 export default class ProgrammeTable extends Component {
+    state = {
+        programmeList: []
+    };
+    componentDidMount() {
+        console.log(MockData.results)
+        this.setState({
+            programmeList: MockData.results
+        })
+    }
     render() {
-        return (
-            <div className="App">
-                <p className="App-intro">
-                    Program Table
-                </p>
-            </div>
-        );
+        if (this.state.programmeList.length > 0) {
+            return (
+                <div>
+                    <Alert color="primary" className="table-alert">
+                        STV — Program Table check it out!
+                    </Alert>
+                    <Table bordered hover responsive className="program-table">
+                        {/* table header */}
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Description </th>
+                                <th>Active Status</th>
+                            </tr>
+                        </thead>grayed out reactstrip
+                        {/* table body */}
+                        <tbody>
+                            {/* table rows */}
+                            {this.state.programmeList.map((programme, i) => (
+                                <tr key={i} className={programme.active?'active':'in-active'}>
+                                    <th scope="row">{i+1}</th>
+                                    <td className="row-name">{programme.name}</td>
+                                    <td className="row-descriptiopn">{programme.shortDescription}</td>
+                                    <td> {programme.active? <span>&#10004;</span> : <span>&#10006;</span> }</td>
+                                 </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </div>
+            );
+        } else {
+            return null
+        }
     }
 }
