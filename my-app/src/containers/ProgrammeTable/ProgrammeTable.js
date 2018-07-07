3ae884d28f0b89e4508fc45754df36e376dafed5
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-07 09:37:33 +0100

    change title and icon

--- my-app/src/containers/ProgrammeTable/ProgrammeTable.js
+++ my-app/src/containers/ProgrammeTable/ProgrammeTable.js
@@ -4,10 +4,9 @@ export default class ProgrammeTable extends Component {
     render() {
         return (
             <div className="App">
-
                 <p className="App-intro">
                     Program Table
-        </p>
+                </p>
             </div>
         );
     }
