d794a473aac472a166462623b8b34acca2dc8dfd
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-07 09:22:00 +0100

    add programme table container

---
+++ my-app/src/containers/ProgrammeTable/ProgrammeTable.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class ProgrammeTable extends Component {
+    render() {
+        return (
+            <div className="App">
+
+                <p className="App-intro">
+                    Program Table
+        </p>
+            </div>
+        );
+    }
+}
