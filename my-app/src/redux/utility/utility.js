d4fd2f5ab2056429b01281584234e6aeb506c9ed
K kz <kkz@Ks-MacBook-Pro.local>
2018-07-08 21:00:11 +0100

    update names

--- my-app/src/redux/utility/utility.js
+++ my-app/src/redux/utility/utility.js
@@ -1,13 +1,13 @@
-const Utility={
-     removeProgramFunc:(progList, id) => {
-        let index;
-        let myList = [...progList];
-        myList.map((program, i) => {
-         return program.id === id ? (index = i) : null;
-        });
-        myList.splice(index, 1);
-        return myList;
-      },
-}
-  
+const Utility = {
+  removeProgrammeFunc: (progList, id) => {
+    let index;
+    let myList = [...progList];
+    myList.map((program, i) => {
+      return program.id === id ? (index = i) : null;
+    });
+    myList.splice(index, 1);
+    return myList;
+  }
+};
+
 export default Utility;
