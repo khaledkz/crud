08d390d75a6bdf56e838efc5aad5d986928bf373
K kz <kkz@Ks-MacBook-Pro.local>
2018-07-12 22:43:53 +0100

    update

--- my-app/src/containers/ProgrameTable/dispatchToProps.js
+++ my-app/src/containers/ProgrameTable/dispatchToProps.js
@@ -1,21 +1,20 @@
-
 import { addNewProgramme } from "../../redux/Actions/programme";
 
 import * as Action from "../../redux/Actions/programme";
 
- const dispatchToProps = dispatch => {
-    return {
-      submitNewProgramme: programme => {
-        dispatch(addNewProgramme(programme));
-      },
-      removeProgramme: ID => {
-        dispatch(Action.removeProgramme(ID));
-      },
-      resetSearchForProgramme: () => dispatch(Action.resetSearchForProgramme()),
-      sortByName: () => dispatch(Action.sortByName()),
-      sortById: () => dispatch(Action.sortById()),
-      searchProgram: programme => dispatch(Action.searchForProgram(programme))
-    };
+const dispatchToProps = dispatch => {
+  return {
+    submitNewProgramme: programme => {
+      dispatch(addNewProgramme(programme));
+    },
+    removeProgramme: ID => {
+      dispatch(Action.removeProgramme(ID));
+    },
+    resetSearchForProgramme: () => dispatch(Action.resetSearchForProgramme()),
+    sortByName: () => dispatch(Action.sortByName()),
+    sortById: () => dispatch(Action.sortById()),
+    searchProgram: programme => dispatch(Action.searchForProgram(programme))
   };
+};
 
-  export default dispatchToProps;
+export default dispatchToProps;
